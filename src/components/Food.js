cd048c20badce4a359d964b1103ff8ba6b94e680
Garima-Manikpuri <108508073+Garima-Manikpuri@users.noreply.github.com>
2023-03-06 17:47:52 +0530

    Update Food.js

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export default (props) => {
-  const style = {
-    left: `${props.dot[0]}%`,
-    top: `${props.dot[1]}%`
-  };
+  const ref = useRef();
 
-  return <div className="snake-food" style={style}></div>;
+  return (
+    <div>
+      {props.snakeDots.map((dot, i) => {
+        const style = {
+          left: `${dot[0]}%`,
+          top: `${dot[1]}%`
+        };
+        return (
+          <div className="snake-dot" ref={ref} key={i} style={style}></div>
+        );
+      })}
+    </div>
+  );
 };
